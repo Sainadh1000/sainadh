50e5c7c7749f9cb35d165b774b49c5ffc7e004b4
Sainadh1000 <leelanagasainadhd@gmail.com>
2025-10-27 21:39:50 +0530

    inheritence

---
+++ inheritence/MRO.py
@@ -0,0 +1,27 @@
+class A:
+    def show(self):
+        print("A Show")
+
+class B():
+    def show(self):
+        print("B Show")
+
+class C(A,B):
+    pass
+
+obj = C()
+obj.show()
+print(C.mro())
+
+# super()
+class Parent:
+    def greet(self):
+        print("Hello From Parent")
+
+class Child(Parent):
+    def greet(self):
+        super().greet()
+        print("hello from child")
+
+child = Child()
+child.greet()
